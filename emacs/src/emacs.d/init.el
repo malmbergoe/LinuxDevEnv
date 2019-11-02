0f8b9fc17c9ec14e1f7c6c611fc21875bab1ea4c
Mats Malmberg <matsmalmberg86@gmail.com>
2019-11-02 19:23:05 +0100

    fresh take on the emacs init file

--- emacs/src/emacs.d/init.el
+++ emacs/src/emacs.d/init.el
@@ -1,13 +1,4 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (tango-dark)))
- '(global-linum-mode t))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(load-theme 'tango-dark)
+(global-linum-mode t)
+(when (fboundp 'windmove-default-keybindings)
+  (windmove-default-keybindings))
